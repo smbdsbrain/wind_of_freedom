a7ab6e885481701b90515b5f839ec17847e7c183
Pavel Kolokdin <pk@napoleonit.ru>
2018-07-03 18:36:20 +0500

    ✨ make some prototype

--- freedom/views.py
+++ freedom/views.py
@@ -8,6 +8,6 @@ GA_TRACKING_ID = os.getenv('GA_TRACKING_ID')
 
 async def wind(request, wind):
     cid = uuid.uuid4()
-    size = 10
     async with GA(GA_TRACKING_ID) as ga:
-        ga.event(str(cid), ec='post', ea='post', ev=size)
+        ga.event(str(cid), ec='post', ea='post', ev=request.content_length)
+    return wind
